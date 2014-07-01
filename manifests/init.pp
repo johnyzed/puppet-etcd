be6a0afa5fd768a7bdc6490f55255063d9f1313e
johnyzed <zenoujohn@gmail.com>
2014-07-01 16:38:57 +0300

    fixing versioning issue, removing go-lang dependency

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,8 +14,12 @@ class etcd(
 	}else {
 		$need_peers = true
 	}
-	
-	$code_repo_url ="${github_url}/archive/v${etcd_version}.tar.gz"
+
+	$low_kernel=downcase($kernel)
+	$code_repo_url ="${github_url}/releases/download/v${etcd_version}/etcd-v${etcd_version}-${kernel}-${architecture}.tar.gz"
+	$etcd_dir = "${install_path}/etcd-v${etcd_version}-${low_kernel}-${architecture}"
+
+	notice($low_arch)
 
 	file { $install_path:
 		ensure => "directory",
@@ -26,15 +30,15 @@ class etcd(
 		path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
 		cwd     => $install_path,
 		require => File[$install_path],
-		creates => "${install_path}/v${etcd_version}.tar.gz"
+		creates => "${install_path}/etcd-v${etcd_version}-${kernel}-${architecture}.tar.gz"
 	}
 
 	exec { "ectd_untar":
-		command => "tar -xvzf ${install_path}/v${etcd_version}.tar.gz",
+		command => "tar -xvzf ${install_path}/etcd-v${etcd_version}-${kernel}-${architecture}.tar.gz",
 		path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin",
 		cwd     => $install_path,
 		require => Exec['etcd_git_dowload'],
-		creates => "${install_path}/etcd-${etcd_version}"
+		creates => $etcd_dir
 	}
 
     exec { "export_path_profile":
@@ -42,23 +46,15 @@ class etcd(
     	path   => [ '/usr/bin', '/bin' ],
     }
 
-	exec { "etcd_build":
-		command => "${install_path}/etcd-${etcd_version}/build",
-		cwd     => "${install_path}/etcd-${etcd_version}",
-		path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/go/bin:/usr/local/go/bin",
-		require => [Exec["ectd_untar"],Exec['export_path_profile']],
-		onlyif  => "test -f ${install_path}/etcd/build", 
-	}
-
 	exec { "config_etcd":
 		command => $need_peers ?{
-			false => "nohup ${install_path}/etcd-${etcd_version}/bin/etcd -peer-addr ${hostname}:${peer_port} -addr ${hostname}:${regular_port} -data-dir machines/${machine_name} -name ${machine_name} -bind-addr 0.0.0.0 &",
-			true  => "nohup ${install_path}/etcd-${etcd_version}/bin/etcd -peer-addr ${hostname}:${peer_port} -addr ${hostname}:${regular_port} -peers ${peers} -data-dir machines/${machine_name} -name ${machine_name} -bind-addr 0.0.0.0 &",
+			false => "nohup ${etcd_dir}/etcd -peer-addr ${hostname}:${peer_port} -addr ${hostname}:${regular_port} -data-dir machines/${machine_name} -name ${machine_name} -bind-addr 0.0.0.0 &",
+			true  => "nohup ${etcd_dir}/etcd -peer-addr ${hostname}:${peer_port} -addr ${hostname}:${regular_port} -peers ${peers} -data-dir machines/${machine_name} -name ${machine_name} -bind-addr 0.0.0.0 &",
 		},
-		cwd     => "${install_path}/etcd-${etcd_version}",
+		cwd     => $etcd_dir,
 		path    => "/usr/bin:/usr/sbin:/bin:/usr/local/bin:/usr/local/go/bin:/usr/local/go/bin",
 		unless  => "/bin/ps -ef |grep -v grep|grep etcd",
-		require => Exec['etcd_build'],
+		require => Exec['ectd_untar'],
 	}
 
 }
